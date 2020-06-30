b4586e0b840b7b18617e6db8483d68313866920b
Asura <Liangz.org@gmail.com>
2020-06-30 21:59:09 +0800

    feat(playsound): playsound

---
+++ src/013-爆破特技窗体/Unit1.dfm
@@ -0,0 +1,24 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 262
+  ClientWidth = 407
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnMouseMove = Label1MouseMove
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label_1: TLabel
+    Left = 152
+    Top = 112
+    Width = 84
+    Height = 13
+    Caption = #19981#35201#38752#36817#25105'~~~'
+  end
+end
---
+++ src/013-爆破特技窗体/Unit1.pas
@@ -0,0 +1,45 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Winapi.MMSystem, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label_1: TLabel;
+    procedure Label1MouseMove(SEnder: TObject; Shift: TShiftState; X,
+      Y: Integer);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+  PtsType = array [0..15, 0..1] of Integer;
+
+var
+  Form1: TForm1;
+const
+  Pts: PtsType = (
+    (0, 0), (800, 0), (800, 600),
+    (200, 600), (200, 220), (300, 280),
+    (265, 205), (350, 117), (205, 170),
+    (120, 90), (130, 200), (60, 350), (200, 220),
+    (200, 600), (0, 600), (0, 0)
+  );
+
+implementation
+
+{$R *.dfm}
+
+Procedure TForm1.Label1MouseMove(SEnder: TObject; Shift: TShiftState; X, Y: Integer);
+var
+  HRegion1: THandle;
+Begin
+  HRegion1 := CreatePolygonRgn (Pts, sizeof (Pts) div 8, alternate);
+  PlaySound('boom.wav', 0, snd_async);
+  SetWindowRgn (Handle, HRegion1, True);
+End;
+
+end.
