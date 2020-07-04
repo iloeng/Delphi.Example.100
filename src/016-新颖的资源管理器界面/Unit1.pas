aabb6e21b45721b6322d26181209828bb302ca16
Asura <Liangz.org@gmail.com>
2020-07-04 23:09:38 +0800

    fix(deploy): user delphi 10.3.3

---
+++ src/016-新颖的资源管理器界面/Unit1.pas
@@ -0,0 +1,35 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.Menus, Vcl.Grids,
+  Vcl.Outline, Vcl.Samples.DirOutln, Vcl.StdCtrls, Vcl.FileCtrl, Vcl.Tabs,
+  Vcl.ExtCtrls;
+
+type
+  TFMForm = class(TForm)
+    DriveTabSet: TTabSet;
+    FileList: TFileListBox;
+    DirectoryOutline: TDirectoryOutline;
+    MainMenu_1: TMainMenu;
+    MenuItem_1: TMenuItem;
+    StatusBar: TStatusBar;
+    Panel_1: TPanel;
+    Panel_2: TPanel;
+  private
+    { Private declarations }
+    procedure ConfirmChange(const ACaption, FromFile, ToFile: string);
+  public
+    { Public declarations }
+  end;
+
+var
+  FMForm: TFMForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ src/016-新颖的资源管理器界面/新颖的资源管理器界面.dpr
@@ -0,0 +1,14 @@
+﻿program 新颖的资源管理器界面;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {FMForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFMForm, FMForm);
+  Application.Run;
+end.
